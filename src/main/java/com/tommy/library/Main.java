1678b8a0d0d7cf69b90150bc18a180fcc1dfec8c
FUJI <jazzmaster204@gmail.com>
2026-05-20 15:18:38 +0300

    hibernate initial comit

---
+++ src/main/java/com/tommy/library/Main.java
@@ -0,0 +1,32 @@
+package com.tommy.library;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg22xml")
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+
+        Session session = factory.openSession();
+
+        session.beginTransaction();
+
+        User user = new User();
+        user.setName("Vano");
+        user.setAge(77);
+
+        session.persist(user);
+
+        session.getTransaction().commit();
+
+        session.close();
+        factory.close();
+
+
+    }
+}
